35f8d77ef2258e91b585ac59fd9cda5a5c9552ca
xXOsielXx <osielhdez597@gmail.com>
2023-11-27 20:05:47 -0500

    Initial commit

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
-alert("Rock Paper Scissors");
+let round = 1;
+let roundTitle = document.querySelector("#round-title");
 
-let playerScore = 0;
-let computerScore = 0;
+let info = document.querySelector(".info");
+
+let playerCounter = 0;
+let computerCounter = 0;
+
+let playerSelection;
+
+let playerSymbol;
+let computerSymbol;
+
+let playerBox = document.querySelector("#player-box");
+let computerBox = document.querySelector("#computer-box");
+
+let playerPara = document.querySelector("#player-para");
+let computerPara = document.querySelector("#computer-para");
+
+let playerScore = document.querySelector("#player-score")
+let computerScore = document.querySelector("#computer-score");
+
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
 
 function getComputerChoice() {
 	let randomNum = Math.floor(Math.random()*3);
@@ -15,22 +36,43 @@ function getComputerChoice() {
 	}
 }
 
-function getPlayerSelection() {
-	while (true) {
-		let playerSelection =  prompt("Choose: Rock Paper Scissors").toLowerCase();
-		
-		if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissors") {
-			return playerSelection
-		}
-		else {
-			true
-		}
-	}
-}
-
 function playRound(playerSelection, computerSelection) {
+	switch(playerSelection) {
+		case "rock":
+			playerSymbol = "✊";
+			break
+		
+		case "paper":
+			playerSymbol = "✋";
+			break
+		
+		case "scissors":
+			playerSymbol = "✌";
+			break
+	}
+
+	switch(computerSelection) {
+		case "rock":
+			computerSymbol = "✊";
+			break
+
+		case "paper":
+			computerSymbol = "✋";
+			break
+		
+		case "scissors":
+			computerSymbol = "✌";
+			break
+	}
+
+	playerBox.textContent = playerSymbol;
+	computerBox.textContent = computerSymbol;
+
+	playerPara.textContent = playerSelection;
+	computerPara.textContent = computerSelection;
+
 	if (playerSelection == computerSelection) {
-		return "Tie"
+		
 	}
 
 	else if (
@@ -39,9 +81,7 @@ function playRound(playerSelection, computerSelection) {
 		(playerSelection == "scissors" && computerSelection == "paper")
 		) {
 
-		playerScore++;
-
-		return "Player win!";
+		playerCounter++;
 	}
 
 	else if (
@@ -50,40 +90,58 @@ function playRound(playerSelection, computerSelection) {
 		(computerSelection == "scissors" && playerSelection == "paper")
 		) {
 
-		computerScore++;
-
-		return "Computer win!";
+		computerCounter++;
 	}
+
+	if (playerCounter >= 5 || computerCounter >=5) {
+		if (playerCounter > computerCounter) {
+			info.textContent = `Congratulations, you win the round ${round}!`;
+		}
+
+		else if (playerCounter < computerCounter) {
+			info.textContent = `Computer wins the round ${round}`;
+		}
+
+		else {
+			info.textContent = `The round ${round} was a tie`;
+		}
+
+		playerBox.textContent = "❔";
+		computerBox.textContent = "❔";
+
+		playerPara.textContent = "";
+		computerPara.textContent = "";
+
+		playerCounter = 0;
+		computerCounter = 0;
+
+		round += 1;
+		roundTitle.textContent = `Round ${round}`;
+	}
+
+	playerScore.textContent = `Player: ${playerCounter}`;
+	computerScore.textContent = `Computer: ${computerCounter}`;
 }
 
 function main() {
-	let counter = 1;
-	while (counter <= 5) {
-		computerChoice = getComputerChoice();
-		console.log(computerChoice);
+	rock.addEventListener("click", () => {
+		playerSelection =  "rock";
+		computerSelection = getComputerChoice();
+		playRound(playerSelection, computerSelection);
+	});
 
-		playerSelection = getPlayerSelection();
-		console.log(playerSelection);
+	paper.addEventListener("click", () => {
+		playerSelection =  "paper";
+		computerSelection = getComputerChoice();
+		playRound(playerSelection, computerSelection);
+	});
 
-		alert(playRound(playerSelection, computerChoice));
-
-		counter++;
-	}
+	scissors.addEventListener("click", () => {
+		playerSelection =  "scissors";
+		computerSelection = getComputerChoice();
+		playRound(playerSelection, computerSelection);
+	});
 }
 
 
 main();
-
-alert("Player " + playerScore + " - " + computerScore + " Computer");
-
-if (playerScore > computerScore) {
-	alert("Congrats, you win this round!");
-}
-
-else if (playerScore < computerScore) {
-	alert("The computer win this round");
-}
-
-else {
-	alert("This round was a tie");
-}
